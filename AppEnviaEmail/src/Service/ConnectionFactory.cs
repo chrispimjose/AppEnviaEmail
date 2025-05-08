de21b16dcd8bceb9652fa13faf4a66e25d6410c1
Caio Souza <ocaiomoises@gmail.com>
2025-05-08 19:54:13 -0300

    Criado: Conexão com banco de dados
    - Inserção de usuários

---
+++ AppEnviaEmail/src/Service/ConnectionFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace AppEnviaEmail.src.Service
+{
+    internal class ConnectionFactory
+    {
+        private static string connectionProperties =
+               "Server=localhost;" +
+               "Port=3306;" +
+               "Database=database_odontologia;" +
+               "User=root;" +
+               "Password=";
+
+        public static MySqlConnection GetConnection()
+        {
+            try
+            {
+                var connection = new MySqlConnection(connectionProperties);
+                connection.Open();
+                return connection;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Erro ao conectar ao banco de dados", e);
+            }
+        }
+    }
+}
